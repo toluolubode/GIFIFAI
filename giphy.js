814410167ceab24865c3675062872e0baedfaa76
Tolu Olubode <toluwanimiolubode@gmail.com>
2017-02-15 17:02:09 -0500

    Clarifai model is live. Now detects sadness and fear. Needs to be trained for anger and happiness. :). Random Cat gifs are also loading!

--- giphy.js
+++ giphy.js
@@ -1,5 +1,3 @@
-//var button = document.getElementById("gifbutton");
-
 $('#gifbutton').on('click', giphy)
 var query;
 
@@ -8,82 +6,28 @@ var b64;
 function visionbase64() {
     // instantiate a new Clarifai app passing in your clientId and clientSecret
     var app = new Clarifai.App(
-        'EFu10OAenQzUcWbL421g6akTv03qKpP1LY-nXTCW',
-        '8uFy-k4onXl3U_pszMJufXKCu4Q3lw3HWqu5cY1E'
+        '3LT42ODvLUE8dxSL8z2LU34EXV4A7rPZvT2yrDRr',
+        'ElxV_fQJeQaZiebwbeK8ifjr55dTE8ja459m2795'
     );
 
-    //TRAIN MY CALRIFAI MODEL BABY!!
-    //    app.inputs.create([
-    //            //PUT TRAINING IMAGES HERE
-    //        {
-    //            url: "https://samples.clarifai.com/metro-north.jpg"
-    //            },
-    //        {
-    //            url: "https://samples.clarifai.com/wedding.jpg"
-    //            }
-    //           concepts: [
-    //            {
-    //                id: "happy",
-    //                value: true
-    //          }
-    //        ]
-    //      ]).then(
-    //        function (response) {
-    //            // do something with response
-    //        },
-    //        function (err) {
-    //            // there was an error
-    //        }
-    //    );
-    //GENERATE A MODEL
-    //    app.models.create(
-    //        "emotion", [
-    //            {
-    //                "id": "happy"
-    //            },
-    //            {
-    //                "id": "sad"
-    //            },
-    //            {
-    //                "id": "anger"
-    //            },
-    //            {
-    //                "id": "fear"
-    //            }
-    //    ]
-    //    ).then(
-    //        function (response) {
-    //            // do something with response
-    //        },
-    //        function (err) {
-    //            // there was an error
-    //        }
-    //    );
-    //TRAIN THE MODEL
-    //    app.models.train("{model_id}").then(
-    //        function (response) {
-    //            // do something with response
-    //        },
-    //        function (err) {
-    //            // there was an error
-    //        }
-    //    );
     var query;
 
-    app.models.predict(Clarifai.GENERAL_MODEL, {
+    app.models.predict("GIF EMOTION", {
         base64: b64
     }).then(
         function (response) {
+            console.log(response);
             let d = response["rawData"]["outputs"][0]["data"]["concepts"];
-            console.log(d);
+            //            console.log(d);
             query = d[0]["name"];
             finalquery = "q=" + query + "+cat"
             console.log(query);
             console.log(finalquery);
-            //            console.log(query.replace(/['"]+/g, ''));
+            console.log(query.replace(/['"]+/g, ''));
+            alert("Hey don't be " + query + ". Click on the GIF ME button and get a CAT GIF")
         },
         function (err) {
-            alert("dammmmnnnn");
+            alert("FUCK MEEEMEMEMEMEM");
             console.log(err);
         }
     );
@@ -97,12 +41,12 @@ function take_snapshot() {
     Webcam.snap(function (data_uri) {
         document.getElementById('my_result').innerHTML = '<img src="' + data_uri + '"/>';
         b64 = data_uri.replace('data:image/jpeg;base64,', "");
-        console.log(b64);
+        //        console.log(b64);
         visionbase64()
         return b64
 
     });
-    console.log(b64);
+    //    console.log(b64);
 }
 
 
@@ -114,17 +58,15 @@ function giphy() {
     //        q = "happy cat"; // search query
 
     request = new XMLHttpRequest;
-    request.open('GET', 'http://api.giphy.com/v1/gifs/search?' + finalquery + '&api_key=dc6zaTOxFJmzC');
+    request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + finalquery);
+
 
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             var p1 = JSON.parse(request.responseText);
-            link = p1.data[1];
+            link = p1.data.fixed_height_downsampled_url;
             console.log(link);
-            giflink = link.images.downsized.url.replace(/['"]+/g, '');
-            console.log(giflink)
-            alert(giflink);
-            document.getElementById("giphyme").innerHTML = '<center><img src = "' + giflink + '"  title="GIF via Giphy"></center>';
+            document.getElementById("giphyme").innerHTML = '<center><img src = "' + link + '"  title="GIF via Giphy"></center>';
         } else {
             console.log('OH NO WE COULD NOT GIF YOU');
         }
