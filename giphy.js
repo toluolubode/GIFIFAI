085872d08c5bcaf3685b5f111c1989b3e313c96d
Tolu Olubode <toluwanimiolubode@gmail.com>
2017-02-22 01:20:09 -0500

    Started support for dogs as well Still training clarifai model

--- giphy.js
+++ giphy.js
@@ -1,7 +1,23 @@
 //$('#gifbutton').on('click', giphy)
 var query;
+var animal = "cat";
 
 var b64;
+
+//Handles cat and dog selection
+$(document).ready(function () {
+    $('input[type=radio][name=optradio]').change(function () {
+        switch ($(this).val()) {
+        case 'cat':
+            swal("Cats! nice choice", "They're pretty aren't they?");
+            break;
+        case 'dog':
+            swal("Puppers! nice choice", "They're pretty aren't they?");
+            animal = "dog";
+            break;
+        }
+    });
+});
 
 function visionbase64() {
     // instantiate a new Clarifai app passing in your clientId and clientSecret
@@ -20,13 +36,13 @@ function visionbase64() {
             let d = response["rawData"]["outputs"][0]["data"]["concepts"];
             //            console.log(d);
             query = d[0]["name"];
-            finalquery = "q=" + query + "+cat"
+            finalquery = "q=" + query + "+" + animal;
             console.log(query);
-            //            console.log(finalquery);
+            console.log(finalquery);
             console.log(query.replace(/['"]+/g, ''));
             if (query == "Happy") {
-                //                swal("Awesome! Be " + query + ". Click on the GIF ME button and spread your happiness to all the sad cats.");
-                finalquery = "q=Sad+cat"
+                //                swal("Awesome! Be " + query + ". Click on the GIF ME button and spread your happiness to all the sad cats."); 
+                finalquery = "q= Sad+" + animal
                 swal({
                     title: "Awesome! Be " + query + ".",
                     text: "Click on the GIF ME button and spread your happiness to all the sad cats.",
@@ -37,6 +53,7 @@ function visionbase64() {
                 }, function () {
                     request = new XMLHttpRequest;
                     request.open('GET', 'https://api.giphy.com/v1/gifs/search?' + finalquery + '&api_key=dc6zaTOxFJmzC&limit=100&rating=pg');
+                    console.log(request);
 
 
                     request.onload = function () {
@@ -66,10 +83,10 @@ function visionbase64() {
 
 
             } else {
-                finalquery = "q=Happy+cat"
+                finalquery = "q=Happy+" + animal;
                 swal({
                     title: "It look's like you are " + query + ".",
-                    text: "I think what can cheer your right up. Click on the GIF me button",
+                    text: "I think I know what can cheer your right up. Click on the GIF me button",
                     type: "success",
                     showCancelButton: true,
                     closeOnConfirm: false,
@@ -112,7 +129,7 @@ function visionbase64() {
         },
         function (err) {
             alert("FUCK MEEEMEMEMEMEM");
-            console.log(err);
+
         }
     );
 
@@ -133,37 +150,3 @@ function take_snapshot() {
     });
     //    console.log(b64);
 }
-
-
-
-
-
-
-//function giphy() {
-//    //        q = "happy cat"; // search query
-//
-//    request = new XMLHttpRequest;
-//    request.open('GET', 'http://api.giphy.com/v1/gifs/search?' + finalquery + '&api_key=dc6zaTOxFJmzC&limit=100&rating=pg');
-//
-//
-//    request.onload = function () {
-//        if (request.status >= 200 && request.status < 400) {
-//            var p1 = JSON.parse(request.responseText);
-//            console.log(p1);
-//            num = Math.floor(Math.random() * 100) + 0
-//            console.log(num);
-//            link = p1.data[num].images.original.url;
-//            //            link = p1.data.fixed_height_downsampled_url;
-//            console.log(link);
-//            document.getElementById("giphyme").innerHTML = '<center><img src = "' + link + '"  title="GIF via Giphy"></center>';
-//        } else {
-//            console.log('OH NO WE COULD NOT GIF YOU');
-//        }
-//    };
-//
-//    request.onerror = function () {
-//        console.log('connection error');
-//    };
-//
-//    request.send();
-//};
